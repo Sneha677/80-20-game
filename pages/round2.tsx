cd7e40a42b93d067146aee430a5cb8107449f1f7
Sneha677 <85390077+Sneha677@users.noreply.github.com>
2022-04-27 12:43:33 +0530

    Add files via upload

---
+++ pages/round2.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+const roundPage: NextPage = () => {
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center py-2 bg-gray-100">
+            <div className="flex min-h-screen flex-col items-center justify-center py-2 bg-gray-100">
+        
+            <div className="overflow-auto bg-gray-200 p-4 mx-16 h-24 text-justify mb-10">
+
+                </div>
+            </div>
+        </div>
+        )
+    }
+    export default roundPage
